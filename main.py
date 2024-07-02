09e0cfa1be32f52dd3131951e2f7b00d6963a4ae
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-07-02 17:47:04 -0500

    backup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # using to host local server
-from flask import Flask, render_template, jsonify, request
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'd42c51f24733b869a5916a8c09043624'
@@ -7,14 +7,9 @@ app.config['SECRET_KEY'] = 'd42c51f24733b869a5916a8c09043624'
 
 @app.route('/', methods=['GET', 'POST'])
 def run():
-    print(request.args.get('counter'))
+    print(request.args.get('counter'))    
     return render_template('index.html')
 
-@app.route("/game")
-def live_game():
-    textlist = ['a','b','c','d']
-    counter = request.args.get('counter')
-    return "<p> " + textlist[counter] + " </p>"
 
 
 if __name__ == '__main__':
