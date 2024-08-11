2185b30d36d9ae5f1046e28e0ef4538ffc57c757
pirgalim <109841774+pirgalim@users.noreply.github.com>
2024-08-11 02:18:52 -0500

    current conditions and hourly forecast

--- weather.js
+++ weather.js
@@ -1,10 +1,11 @@
 async function call() {
     try {
-        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=49.6363&longitude=-97.1307&current=temperature_2m,precipitation&hourly=temperature_2m,dew_point_2m,precipitation_probability,cloud_cover,visibility&daily=sunrise,sunset&timezone=America%2FChicago&past_days=1&forecast_days=3');
+        const response = await fetch('https://api.open-meteo.com/v1/forecast?latitude=49.6363&longitude=-97.1307&current=temperature_2m,precipitation,cloud_cover,wind_speed_10m,wind_direction_10m&hourly=temperature_2m,precipitation_probability,cloud_cover&daily=sunrise,sunset&timezone=America%2FChicago&past_days=1&forecast_days=3');
         const data = await response.json();
         return data;
     } catch (error) {
 
+        alert("Error Retrieving Forecast");
         console.error('Error:', error);
     }
 }
@@ -24,85 +25,205 @@ async function display() {
 
 
 
-    var currentArray = Object.values(data["current"]);
-    var currentUnitsArray = Object.values(data["current_units"]);
+    currentCondtions(data);
 
-    var elements = document.getElementsByClassName("current");
+    hourlyConditions(data);
+}
 
 
 
+
+
+
+
+function hourlyConditions(data) {
+
+
+    const hourly = data["hourly"];
+    const hourlyUnits = data["hourly_units"];
+    const currTime = data["current"]["time"];
+
+
+    const times = hourly["time"];
+
+    const clouds = hourly["cloud_cover"];
+    const cloudUnits = hourlyUnits["cloud_cover"];
+
+    const precipitation = hourly["precipitation_probability"];
+    const precipitationUnits = hourlyUnits["precipitation_probability"];
+
+    // const 
+
+    
+
+
+    // convert current time from YYYY-MM-DDTHH:MM to a number representing the current hour
+    var currHour = currTime.split("T")[1].split(":")[0];
+    // convert current time from YYYY-MM-DDTHH:MM to a number representing the current day
+    var currDay = currTime.split("T")[0].split("-")[2];
+
+    console.log(currHour, currDay);
+
+
+
+    var startPosition = 0;
+
+
+
+    for(let i = 0; i < times.length; i++) {
+
+        if( times[i].split("T")[1].split(":")[0] == currHour && times[i].split("T")[0].split("-")[2] == currDay) { startPosition = i; }
+        times[i] = times[i].split("T")[1];
+    }
+
+
+    console.log(times);
+    console.log(currTime);
+
+    console.log(startPosition);
+
+    console.log(times[startPosition])
+
+
+
+
+
+    // obtain list of headings that are part of the 'block-small' class
+    const elements = document.getElementsByClassName("block-small");
+    
 
     for(let i = 0; i < elements.length; i++) {
 
-            if(currentUnitsArray[i] != "seconds") {
-                var body = document.createElement('p');
+        body = document.createElement('p');
+        body.textContent = times[startPosition + i];
+        elements[i].appendChild(body);
 
-                if(currentUnitsArray[i] != "iso8601") {
-                    body.textContent = currentArray[i] + " " + currentUnitsArray[i]
-                }
-                else {
-                    body.textContent = currentArray[i].split("T")[1]
-                }
+        body = document.createElement('br');
+        elements[i].appendChild(body);
+        body = document.createElement('br');
+        elements[i].appendChild(body);
 
-                
-                console.log(body.textContent);
-                elements[i].appendChild(body);
-            }
+        body = document.createElement('p');
+        body.textContent = clouds[startPosition + i] + " " + cloudUnits;
+        elements[i].appendChild(body);
+
+        body = document.createElement('br');
+        elements[i].appendChild(body);
+        body = document.createElement('br');
+        elements[i].appendChild(body);
+        
+        body = document.createElement('p');
+        body.textContent = precipitation[startPosition + i] + " " + precipitationUnits;
+        elements[i].appendChild(body);
 
 
-            
     }
 
 
 
 
+    // console.log(currHour % 6);
 
+    // slice = currHour % 6;
 
-    
+    // var bottom =  slice * 4 -1;
+    // var top = slice*4 + 5;
 
+    // console.log(bottom, top);
 
-
-
-
-    // var elements = document.getElementsByClassName("current");
-    // var counter = 0; 
-    // for(const key in data["current"]) {
-
-    //     if(key != "interval") {
-    //         console.log(data["current"][key]);
-
-    //         var body = document.createElement('p');
-    //         body.textContent = data["current"][key];
-    //         console.log(body.textContent);
-    //         elements[counter].appendChild(body);
-
-    //         counter++;
-    //     }
-        
-    // }
-
-
+    // console.log( times.slice(bottom, top) )
 
 }
 
 
 
-// function newPopulate(data, className, category, delim) {
-
-//     var elements = document.getElementsByClassName(className);
-
-//     var currentArray = Object.values(data[className]);
-//     var currentUnitsArray = Object.values(data[className + "_units"]);
 
 
+function windDirection(data) {
 
-//     for(let i = 0; i < elements.length; i++) {        
-//             var body = document.createElement('p');
-//             body.textContent = currentArray[i] + " " + currentUnitsArray[i]
-//             console.log(body.textContent);
-//             elements[i].appendChild(body);  
-//     }
-// }
+    
+    const direction = Number( data["current"]["wind_direction_10m"] );
+
+
+    if(direction >= 337.5 && direction < 22.5) { return "N"; }
+    if(direction >= 22.5 && direction < 67.5) { return "NE"; }
+    if(direction >= 67.5 && direction < 112.5) { return "E"; }
+    if(direction >= 112.5 && direction < 157.5) { return "SE"; }
+    if(direction >= 157.5 && direction < 202.5) { return "S"; }
+    if(direction >= 202.5 && direction < 247.5) { return "SW"; }
+    if(direction >= 247.5 && direction < 292.5) { return "W"; }
+    if(direction >= 292.5 && direction < 337.5) { return "NW"; }
+   
+    return "?";
+}
+
+
+
+
+
+
+
+
+
+function currentCondtions(data) {
+
+    // const currentArray = Object.values(data["current"]);
+    // const currentUnitsArray = Object.values(data["current_units"]);
+
+
+    const current = data["current"];
+    const currentUnits = data["current_units"];
+
+
+    // obtain list of headings that are part of the 'current' class
+    const elements = document.getElementsByClassName("current");
+    // expected output??
+
+
+    var body;
+
+
+    // console.log(elements[0]);
+
+
+    
+
+    // temperature
+    body = document.createElement('p');
+    body.textContent = current["temperature_2m"] + " " + currentUnits["temperature_2m"];
+    elements[0].appendChild(body);
+
+    // cloud cover
+    body = document.createElement('p');
+    body.textContent = current["cloud_cover"] + " " + currentUnits["cloud_cover"];
+    elements[1].appendChild(body);
+
+    // precipitation
+    body = document.createElement('p');
+    body.textContent = current["precipitation"] + " " + currentUnits["precipitation"];
+    elements[2].appendChild(body);
+
+    // precipitation
+    body = document.createElement('p');
+    body.textContent = current["wind_speed_10m"] + " " + currentUnits["wind_speed_10m"] + " " + windDirection(data);
+    elements[3].appendChild(body);
+
+    // timestamp from last request
+    body = document.createElement('p');
+    var time = current["time"].split("T");
+    body.textContent = time[0] + " at " + time[1] + " " + data["timezone_abbreviation"];
+    elements[4].appendChild(body);
+    
+
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -122,7 +243,7 @@ function populate(data, className, category, delim) {
             var dateArray = data[category][className][i].split(delim);
             body.textContent = dateArray[1]
 
-            console.log(body.textContent);
+            // console.log(body.textContent);
 
             elements[i].appendChild(body);
         }
